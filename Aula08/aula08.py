9ab1e417eaa71d79ca547ccaa7c645ef5e598149
dionisio300 <dionisioassis80@gmail.com>
2026-04-25 14:40:45 -0300

    aula8

---
+++ Aula08/aula08.py
@@ -0,0 +1,45 @@
+# import meus_calculos
+
+# areaQuadrado = meus_calculos.areaQuadrado(3)
+# print(f'A área do quadrado é {areaQuadrado}')
+
+# areaT = meus_calculos.areaTriangulo(10,5)
+# print(f'A área do triangulo é {areaT}')
+
+# from meus_calculos import areaTrapezio, areaRetangulo
+
+# trapezio = areaTrapezio(10,8,5)
+# retangulo = areaRetangulo(3,7)
+
+# print(f'A área do trapezio é {trapezio}')
+# print(f'A área do retangulo é {retangulo}')
+
+# from meus_calculos import *
+
+# triangulo = areaTriangulo(3,8)
+# print(f'A área do triangulo é {triangulo}')
+
+# import meus_calculos as areas
+# import meus_alertas as alert
+
+# triangulo = areas.areaTriangulo(10,5)
+# print(f'A área do triangulo é {triangulo}')
+
+# alerta = alert.alertaEstoque(60)
+# print(alerta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
